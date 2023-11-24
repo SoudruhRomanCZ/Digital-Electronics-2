30dd0f9eb2ddd34897c7ffd24b13795d0abd5af6
SoudruhRomanCZ <soudruhromancz@gmail.com>
2023-11-24 21:10:56 +0100

    added RTC and OLED, RTC must be implemented to OLE

--- Projekt/src/main.c
+++ Projekt/src/main.c
@@ -20,14 +20,27 @@ struct Values_structure {
    uint8_t hum_dec;
    uint8_t temp_int;
    uint8_t temp_dec;
-   uint16_t mois_int;
-   uint8_t time_int;
    uint8_t checksum;
 } dht12;
+// Declaration of "rtc" variable with structure "RTC_values_structure"
+struct RTC_values_structure {
+    uint8_t secs;
+    uint8_t mins;
+    uint8_t hours;
+    uint8_t days;
+    uint8_t months;
+    uint8_t years;
+} rtc;
+uint16_t mois_int;
 // Normal values of moisture sensor
 const int AirValue = 620;   //you need to replace this value with Value_1
 const int WaterValue = 310;  //you need to replace this value with Value_2
+// (AirValue - WaterValue)/100; meaning 1% is 2.5 of measured value from 0 to 1023 ex. 310 adn 620 of measuring 
+contst float onePercent = (AirValue - WaterValue)/100; //is it working that way? should be 2.5
+//const float onePercent = 2.5;
 
+// Variable for conversion of moisture sensor data to percentual value
+int percentualValue;
 // Flag for printing new data from sensor
 volatile uint8_t new_sensor_data = 0;
 
@@ -46,14 +59,21 @@ volatile uint8_t new_sensor_data = 0;
 #define soil PC0
 
 int main(void)
-{/*
-    oled_init(OLED_DISP_ON);
-    oled_clrscr();
-
-    // oled_gotoxy(x, y)
+{
+    //boot screen
+    oled_charMode(DOUBLESIZE);  //would rather write it on 1 line on the top of the screen
+    oled_puts("SMART");
     oled_gotoxy(0, 2);
-    oled_puts("128x64, SHH1106");
-*/
+    oled_puts("PLANT");
+    oled_gotoxy(0, 4);
+    oled_puts("WATERING");
+    oled_gotoxy(0, 6);
+    oled_puts("SYSTEM");
+    oled_display();
+
+
+    oled_charMode(NORMALSIZE);
+
     char string[2];  // For converting numbers by itoa()
 
     twi_init();
@@ -93,30 +113,64 @@ int main(void)
     
     while (1) {
         if (new_sensor_data == 1) {
+            oled_clrscr(); //it will clear the first message wouldnt the message be there all the time? so load it again
+            // wouldnt be better to create a function that writes data to oled and uart separatly?
+            //writing an integer value of temperature
             itoa(dht12.temp_int, string, 10);
             uart_puts(string);
             uart_puts(".");
-            
+            oled_gotoxy(0, 2);
+            oled_puts(string);
+
+            //writing an decimal value of temperature
             itoa(dht12.temp_dec, string, 10);
             uart_puts(string);
             uart_puts(" °C ");
-            
-            // Convert "value" to "string" and display it
-            itoa(dht12.mois_int, string, 10);
+            oled_gotoxy(2, 2);
+            oled_puts(".");
+            oled_gotoxy(3, 2);
+            oled_puts(string);
+            oled_gotoxy(4, 2);
+            oled_puts(" °C ");
+
+        	// Display RTC data
+            itoa(rtc.hours, string, 16);
             uart_puts(string);
-            
-            if (dht12.mois_int>850){
+            uart_puts(":");
+            itoa(rtc.mins, string, 16);
+            uart_puts(string);
+            uart_puts(":");
+            itoa(rtc.secs, string, 16);
+            uart_puts(string);
+            uart_puts(":\t");
+
+            // writing an procentual value of moisture
+            itoa(mois_int, string, 10);
+            uart_puts(string);
+            oled_gotoxy(0, 3);
+            oled_puts(string);
+            oled_gotoxy(3, 3);
+            oled_puts("percent");
+
+            if (mois_int>850){
                 uart_puts(": Rostlina má nedostatek vláhy, zapínám zalévání\r\n");
+                oled_gotoxy(0, 4);// can we create some function of animated image on oled?
+                oled_puts("Watering");
                 }
-            else if(dht12.mois_int>750){
+            else if(mois_int>750){
                 uart_puts(": Rostlina má dostatek vláhy, zalévání nejede\r\n");
+                oled_gotoxy(0, 4);
+                oled_puts("adekvatna vlaha");    
                     }
-            else if(dht12.mois_int>700){
+            else if(mois_int>700){
                 uart_puts(": Rostlina má dostatek vláhy, vypínám zalévání\r\n");
+                oled_gotoxy(0, 4);
+                oled_puts("preliata");
                     }
             else {uart_puts(": Chyba snímače, hodnota mimo rozsah měření\r\n");
                     }
-            
+            oled_display();
+
             // Do not print it again and wait for the new data
             new_sensor_data = 0;
             }
@@ -147,6 +201,28 @@ ISR(TIMER1_OVF_vect)
         new_sensor_data = 1;
     }
     twi_stop();
+
+
+
+    // Read Time from RTC DS3231; SLA = 0x68
+        // Test ACK from RTC
+        twi_start();
+        if (twi_write((RTC_ADR<<1) | TWI_WRITE) == 0) {
+            // Set internal memory location
+            twi_write(RTC_SEC_MEM);
+            twi_stop();
+            // Read data from internal memory
+            twi_start();
+            twi_write((RTC_ADR<<1) | TWI_READ); //how the F it knows what data to read from its registers to get this numbers?
+            rtc.secs = twi_read(TWI_ACK);
+            rtc.mins = twi_read(TWI_ACK);
+            rtc.hours = twi_read(TWI_ACK);
+            rtc.days = twi_read(TWI_ACK);
+            rtc.months = twi_read(TWI_ACK);
+            rtc.years = twi_read(TWI_ACK);
+
+        }
+        twi_stop();
 }
 ISR(ADC_vect)
 {
@@ -154,7 +230,8 @@ ISR(ADC_vect)
 
     // Read converted value
     // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
-    dht12.mois_int = ADC;
-    // Convert "value" to "string" and display it
-    itoa(dht12.mois_int, string, 10);
+    mois_int = ADC;
+    //was there itoa of mois_int to string
 }
+
+
